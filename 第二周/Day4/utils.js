356490fc0d556941690f1348100a922514276f2c
liwenli <102495553@qq.com>
2017-07-08 18:12:24 +0800

    update sort

--- 第二周/Day4/utils.js
+++ 第二周/Day4/utils.js
@@ -3,18 +3,24 @@
  */
 var utils = (function () {
     function listToArray(likeAry) {
-        try{
+        try {
             return [].slice.call(likeAry, 0);
-        }catch (e) { // 只有try里面报错 才会执行catch里面的
+        } catch (e) { // 只有try里面报错 才会执行catch里面的
             var newArr = [];
-            for(var i = 0; i < likeAry.length; i++){
+            for (var i = 0; i < likeAry.length; i++) {
                 newArr[newArr.length] = likeAry[i];
             }
             return newArr;
         }
     }
 
+    function jsonParse(data) { // 将json字符串转化为json对象兼容写法
+        return "JSON" in window ? JSON.parse(data) : eval('(' + data + ')')
+    }
+
+
     return {
-        listToArray: listToArray
+        listToArray: listToArray,
+        jsonParse: jsonParse
     }
 })();
