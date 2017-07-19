7a7521738ca254f9fff7f30ae736ae6492158bfb
liwenli <102495553@qq.com>
2017-07-19 17:34:38 +0800

    update 多动画

--- 第四周/Day2/swiper/index2.js
+++ 第四周/Day2/swiper/index2.js
@@ -2,7 +2,9 @@
     // 获取操作元素
     var outer = document.getElementById('outer');
     var inner = document.getElementById('inner');
+    var focus = document.getElementById('focus');
     var oImgs = inner.getElementsByTagName('img');
+    var focusList = focus.getElementsByTagName('li');
 
     // 获取到左右切换按钮
     var btns = outer.getElementsByTagName('a');
@@ -22,7 +24,7 @@
     // 再给动画元素 定义定时器属性时 注意：animate里会给动画元素自动添加个属性ele._timer的定时器
 
     // 自动轮播
-    // inner.timer = setInterval(autoMove, 2000);
+    inner.timer = setInterval(autoMove, 2000);
 
     // autoMove 执行一次 就会切换一次 所以 切换图片 主要是控制autoMove
     function autoMove(n) { // n 传递索引是几 step = n 就让当前切换到 索引为n的那张图片位置
@@ -34,14 +36,14 @@
             utils.css(inner, 'left', 0); // 立马变成第一张(第一张和最后一张一模一样 在变的过程中看不出来 用户会把最后一行当做第一张)
             step = 1; // 紧接着我们 开始 切换到 第二张
         }
-        console.log(step);
         $animate({
             ele: inner,
             target: {
                 left: step * -wid
             },
             duration: 300 // 切换到下一张的过渡动画时间
-        })
+        });
+        changBg(step); // 实现焦点同步
     }
 
     // step++ 往左平移  step-- 往右平移
@@ -59,6 +61,25 @@
         }
         autoMove(step);
     };
+
+    // 焦点
+    for(var i = 0; i < focusList.length; i++){
+        var curLi = focusList[i];
+        curLi.n = i;
+        curLi.onclick = function () {
+            changBg(this.n);
+            autoMove(this.n);
+        }
+    }
+
+    // 切换焦点选中样式
+    function changBg(n) {
+        n >= len - 1? n = 0 : null;
+        for(var i = 0; i < focusList.length; i++){
+            var oLi = focusList[i];
+            i === n ? utils.addClass(oLi, 'select') : utils.removeClass(oLi, 'select');
+        }
+    }
 
     // 滑入滑出控制
     outer.onmouseover = function () {
