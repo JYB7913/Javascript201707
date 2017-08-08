ac70e5f66a5d05c49596b9c9ee06fdb49d70b8c2
liwenli <102495553@qq.com>
2017-08-08 18:20:13 +0800

    update HTML5

---
+++ 第七周/Day1/cookie.js
@@ -0,0 +1,14 @@
+
+// 根据指定 key 从cookie中获取 value
+function getCookie(key) {
+    let str = document.cookie;
+    let reg = /([^=; ]+)=([^=; ]+)/g;
+    let obj = {};
+    str.replace(reg, (a, b, c) => obj[b] = c);
+    return obj[key];
+}
+
+// 根据指定key保存相应value
+function setCookie(key, val) {
+    document.cookie = `${key}=${val}`;
+}
